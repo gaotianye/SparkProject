9e6b81c9af31dcd88fa8f086dfd22d3ad4877429
gaotianye <15156506755@163.com>
2017-05-05 08:18:23 +0800

    add：测试Dao

---
+++ SparkProject/javaProject/src/main/java/cn/gaotianye/javaProject/test/TaskDAOTest.java
@@ -0,0 +1,20 @@
+package cn.gaotianye.javaProject.test;
+
+import cn.gaotianye.javaProject.dao.ITaskDAO;
+import cn.gaotianye.javaProject.dao.factory.DAOFactory;
+import cn.gaotianye.javaProject.domain.Task;
+
+/**
+ * 任务管理DAO测试类
+ * @author Administrator
+ *
+ */
+public class TaskDAOTest {
+	
+	public static void main(String[] args) {
+		ITaskDAO taskDAO = DAOFactory.getTaskDAO();
+		Task task = taskDAO.findById(2);
+		System.out.println(task.getTaskName());  
+	}
+	
+}
